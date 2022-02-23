63d0a7a0d94caba8373bb79741a539e66d3b8414
jayu1023 <73164366+jayu1023@users.noreply.github.com>
2022-02-23 09:28:21 +0530

    Add files via upload

--- app/src/main/java/com/daily/image/pdf/imagetopdfconvert/activity/loginscreen.java
+++ app/src/main/java/com/daily/image/pdf/imagetopdfconvert/activity/loginscreen.java
@@ -1,19 +1,37 @@
 package com.daily.image.pdf.imagetopdfconvert.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.daily.image.pdf.imagetopdfconvert.R;
-import com.daily.image.pdf.imagetopdfconvert.databinding.ActivityLoginscreenBinding;
-import com.daily.image.pdf.imagetopdfconvert.databinding.ActivityMainBinding;
+import com.daily.image.pdf.imagetopdfconvert.adapter.PageAdapter;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 
 public class loginscreen extends AppCompatActivity {
-ActivityLoginscreenBinding loginBinding;
+
+    TabLayout tabLayout;
+//    TabItem tabItem1,tabItem2;
+    ViewPager viewPager;
+//    PageAdapter pageAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        loginBinding = DataBindingUtil.setContentView(loginscreen.this, R.layout.activity_loginscreen);
+        setContentView(R.layout.activity_loginscreen);
+
+        tabLayout = findViewById(R.id.tablayout1);
+//        tabItem1 = findViewById(R.id.tab1);
+//        tabItem2 = findViewById(R.id.tab2);
+        viewPager = findViewById(R.id.vpager);
+
+        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
